f78b7b2f5a47cf643ae4ebd365b2945442d5c4df
Naveen P M <naveen.pm@moveinsync.com>
2023-10-31 17:26:20 +0530

    Added user details section in navgation drawer

--- app/src/main/java/com/dk24/moneycontrol/composables/MainViewComposable.kt
+++ app/src/main/java/com/dk24/moneycontrol/composables/MainViewComposable.kt
@@ -1,13 +1,20 @@
 package com.dk24.moneycontrol.composables
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -17,6 +24,7 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -27,7 +35,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dk24.moneycontrol.R
 import com.dk24.moneycontrol.models.NavigationItem
 import kotlinx.coroutines.launch
 
@@ -46,6 +60,7 @@ fun MainViewCompose(
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
+                UserDetailsNavigationDrawerItem()
                 Spacer(modifier = Modifier.height(16.dp))
                 navigationItems.forEachIndexed { index, item ->
                     NavigationDrawerItem(
@@ -96,5 +111,48 @@ fun MainViewCompose(
                 )
             }
         }
+    }
+}
+
+@Composable
+fun UserDetailsNavigationDrawerItem() {
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .defaultMinSize(minHeight = 46.dp)
+            ) {
+                Text(text = "Naveen PM", style = MaterialTheme.typography.titleLarge)
+                Text(text = "pmnaveeen6@gmail.com", style = MaterialTheme.typography.titleSmall)
+            }
+        }
+        Divider(
+            color = MaterialTheme.colorScheme.secondary,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4_XL)
+@Composable
+fun ComposePreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        UserDetailsNavigationDrawerItem()
     }
 }
