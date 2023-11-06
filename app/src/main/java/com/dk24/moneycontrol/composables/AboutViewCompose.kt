52c53861202cf411199e16a773c7324b359ca45c
Naveen P M <naveen.pm@moveinsync.com>
2023-11-06 12:05:29 +0530

    Added few details in about page

--- app/src/main/java/com/dk24/moneycontrol/composables/AboutViewCompose.kt
+++ app/src/main/java/com/dk24/moneycontrol/composables/AboutViewCompose.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dk24.moneycontrol.enums.TopBarNavigationType
